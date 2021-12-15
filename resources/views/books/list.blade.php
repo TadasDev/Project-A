c503a4e3b00a88ad3a56d9031c7d05a54cacc9af
Tadas <labutis1991@gmail.com>
2021-12-15 16:42:14 +0200

    fixed price range sort

--- resources/views/books/list.blade.php
+++ resources/views/books/list.blade.php
@@ -16,20 +16,6 @@
                         Add new book
                     </button>
                 </a>
-                <div class="w-1/4 flex flex-wrap justify-between">
-                    <select class="p-1 rounded w-3/4">
-                        <option>Lowest (price)</option>
-                        <option>Highest (price)</option>
-                        <option>Newest</option>
-                        <option>Oldest</option>
-                    </select>
-                    <button
-                        type="button"
-                        class=" text-sm bg-green-800 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Select
-                    </button>
-                </div>
-
             </div>
             <x-success-message/>
             <div class="grid grid-cols-5 grid-flow-col  overflow-hidden">
@@ -38,40 +24,34 @@
                     Price range:
                     <ul class="flex ">
                         <form method="GET" action="{{ route('books.sortByPriceRange') }}">
-                        @csrf
+                            @csrf
                             <div>
                                 <x-label for="price_from" :value="__('From')"/>
 
                                 <x-input id="price_from" class="block mt-1 w-full" type="number" name="price_from"/>
                             </div>
-
-                            <!-- Email Address -->
                             <div class="mt-4">
                                 <x-label for="price_to" :value="__('To')"/>
-
-                                <x-input id="price_to" class="block mt-1 w-full" type="number" name="price_to"/>
+                                <x-input id="price_to" step="5" class="block mt-1 w-full" type="number"
+                                         name="price_to"/>
                             </div>
+                            <div class="mt-4">
+                                <x-label for="sort" :value="__('Sort by')"/>
+                                <select class="block mt-1 w-full rounded">
+                                    <option>Lowest (price)</option>
+                                    <option>Highest (price)</option>
+                                    <option>Newest</option>
+                                    <option>Oldest</option>
+                                </select>
+                            </div>
+
                             <div style="margin-top: 25px ; ">
-                                <x-button class="ml-4">
+                                <x-button>
                                     {{ __('Sort') }}
                                 </x-button>
                             </div>
                         </form>
-
-{{--                        <li> From--}}
-{{--                            <x-input type="number" id="price_from" name="price_from" class="w-11/12 p-1  "/>--}}
-{{--                        </li>--}}
-{{--                        <li> To--}}
-{{--                            <x-input type="number" id="price_to" name="price_to" class="w-11/12 p-1 "/>--}}
-{{--                        </li>--}}
-{{--                    </ul>--}}
-{{--                    <a href="{{ route('books.sortByPriceRange') }}">--}}
-{{--                    <button--}}
-{{--                            type="button"--}}
-{{--                            class="w-5/12 text-sm bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline">--}}
-{{--                            Select--}}
-{{--                        </button>--}}
-{{--                    </a>--}}
+                    </ul>
                 </div>
                 <div
                     class="grid grid-cols-1 col-span-4 gap-y-4 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-4">
---
+++ resources/views/books/single.blade.php
@@ -0,0 +1,70 @@
+<x-app-layout>
+    <x-slot name="header">
+
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <!-- component -->
+                    <x-validation-errors class="mb-4" :errors="$errors"/>
+                    <x-success-message/>
+
+                    <div class="flex">
+                        @if($book->images()->count()>0)
+                            <img src="{{ asset('storage/' . $book->images()->first()->file_path) }}"
+                                 style="height: 800px; width: 800px"
+                                 alt=" Post image "
+                                 class="w-full h-full object-center object-cover group-hover:opacity-75"/>
+
+                        @else
+                            <img
+                                style="height: 292px; width: 292px"
+                                src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
+                                alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                                class=" object-center object-cover group-hover:opacity-75">
+
+                        @endif
+                        <div class="flex-col w-full h-full text-center">
+                            <div class="p-6 text-xl">
+                                Book title: {{$book->title}}
+                            </div>
+                            <div class="p-6 text-xl">
+                                Price: ${{$book->price}}
+                            </div>
+                            <div class="p-6 text-xl">
+                                Description: {{$book->description}}
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="p-4">
+                        <x-button class=" p-4">
+                            {{ __('Add to basket') }}
+                        </x-button>
+                    </div>
+
+                </div>
+
+
+            {{--                    <form method="POST" action="{{  }}" enctype="multipart/form-data" class="w-3/4 container align-middle " >--}}
+            @csrf
+
+            <!-- Commnent -->
+                <div class="mt-4 p-4">
+                    <x-label for="Comment" :value="__('Comment')"/>
+
+                    <x-input id="comment" class="block mt-1 w-full h-40" type="textarea" name="title" />
+                </div>
+                <div class="p-4">
+                    <x-button class=" p-4">
+                        {{ __('Add commnet') }}
+                    </x-button>
+                </div>
+                {{--                    </form>--}}
+            </div>
+        </div>
+    </div>
+    </div>
+</x-app-layout>
